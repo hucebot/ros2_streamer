e1fe3e937f7f2a15c97b1da2ecb9b660cf3320c7
Clemente Donoso <clemente.donosok@gmail.com>
2024-10-03 14:26:31 +0200

    Add support for Logitech C930e camera and improve camera configuration
    - Added functionality to support the Logitech C930e camera alongside the Insta360
    - Created a configuration file to manage multiple cameras and their respective parameters
    - Developed a new launch file to start independent nodes for each camera
    - Modified `gstream_service` code to dynamically apply parameters and build the correct pipeline for each camera
    - Updated `udev rules` file to prepare for future integration of serial port rules for each device

--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,6 @@ docker run  \
     --privileged \
     -it \
     -v /dev:/dev \
-    -v $HOME/Desktop/Inria/ROS2:/ros2_ws \
+    -v $HOME/ros2_ws:/ros2_ws \
     -w /ros2_ws \
     ros2_streamer:latest
